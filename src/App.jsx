44065e4466fa666265d011a69c70d3a185e607f9
Sai Harini <saiharini.kandula.s.143@kalvium.community>
2026-02-23 11:14:03 +0530

    Initial React setup with Vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>QuickCart</h1>
+    </div>
+  );
+}
+
+export default App
